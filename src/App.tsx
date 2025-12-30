de7c530cae0e6c5897be8dc67220d78a2da9b2b7
wahid <wahid@7gfuse.com>
2025-12-30 16:06:44 +0500

    updated routes

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,35 @@ function App() {
       <Routes>
         <Route element={<MainLayout />}>
           <Route path="/" element={<FindJobs />} />
+          <Route
+            path="/companies"
+            element={<div>Top Companies Page</div>}
+          />
+
+          <Route
+            path="/tracker"
+            element={<div>Job Tracker Page</div>}
+          />
+
+          <Route
+            path="/calendar"
+            element={<div>My Calendar Page</div>}
+          />
+
+          <Route
+            path="/documents"
+            element={<div>Documents Page</div>}
+          />
+
+          <Route
+            path="/messages"
+            element={<div>Messages Page</div>}
+          />
+
+          <Route
+            path="/notifications"
+            element={<div>Notifications Page</div>}
+          />
         </Route>
       </Routes>
     </BrowserRouter>
